e950069e592ebd9dc033bf30836e32b5fe2043f7
Сергей <gan7er@yandex.ru>
2021-04-17 12:51:29 +0300

    Начало

---
+++ Task-1/Task-1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+      public static void PrimeNomber()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
