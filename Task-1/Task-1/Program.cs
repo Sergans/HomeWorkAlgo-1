80ca51315ee3afa60ab1d1071920812043341d74
Сергей <gan7er@yandex.ru>
2021-04-18 10:39:22 +0300

    Без теста

--- Task-1/Task-1/Program.cs
+++ Task-1/Task-1/Program.cs
@@ -4,12 +4,56 @@ namespace Task_1
 {
     class Program
     {
-      public static void PrimeNomber()
+      public static void PrimeNomber(int nomber)
         {
+            if (nomber > 0)
+            {
+                int d = 0;
+                for (int i = 2; i < nomber; i++)
+                {
+                    for (; nomber % i == 0; i++)
+                    {
+                        d++;
+                    }
+
+                }
+                if (d == 0)
+                {
+                    Console.WriteLine($"{nomber} - Простое число");
+                }
+                else
+                {
+                    Console.WriteLine($"{nomber} - Не проcтое число");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Введенное число меньше или равно 0");
+            }
+            
+            
+
+
 
         }
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Введите целое положительное число");
+            try
+            {
+                PrimeNomber(Convert.ToInt32(Console.ReadLine()));
+
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка: Введеное значение не является целым или не являтся числом");
+            }
+
+
+            
+
             
         }
     }
