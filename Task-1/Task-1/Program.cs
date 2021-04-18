19b1365689b96fb930e4a072d05e6489c6375b1e
Сергей <gan7er@yandex.ru>
2021-04-18 12:09:03 +0300

    С тестом

--- Task-1/Task-1/Program.cs
+++ Task-1/Task-1/Program.cs
@@ -4,11 +4,32 @@ namespace Task_1
 {
     class Program
     {
-      public static void PrimeNomber(int nomber)
+        public class TestCase
         {
+            public int nomberd { get; set; }
+            public int testnomber { get; set; }
+        }
+
+       static void TestReturn(TestCase testCase)
+        {
+            if (testCase.nomberd == PrimeNomber(testCase.testnomber))
+            {
+                Console.WriteLine("VALID TEST");
+            }
+            else
+            {
+                Console.WriteLine("INVALID TEST");
+
+            }
+
+        }
+
+      public static int PrimeNomber(int nomber)
+        {
+            int d = 0;
             if (nomber > 0)
             {
-                int d = 0;
+
                 for (int i = 2; i < nomber; i++)
                 {
                     for (; nomber % i == 0; i++)
@@ -31,15 +52,21 @@ namespace Task_1
             {
                 Console.WriteLine("Введенное число меньше или равно 0");
             }
-            
-            
+
+            return d; 
 
 
 
         }
         static void Main(string[] args)
         {
-            
+            var testCase1 = new TestCase() { nomberd = 0, testnomber = 3 };
+            var testCase2 = new TestCase() { nomberd = 1, testnomber = 4 };
+            TestReturn(testCase1);
+            TestReturn(testCase2);
+
+
+
             Console.WriteLine("Введите целое положительное число");
             try
             {
