b6a901f3f8dc1f10a05d8ad7ecf649b60de0bd9a
Сергей <gan7er@yandex.ru>
2021-04-18 14:05:38 +0300

    Рекурсия

--- Task-3/Task-3/Program.cs
+++ Task-3/Task-3/Program.cs
@@ -4,9 +4,26 @@ namespace Task_3
 {
     class Program
     {
+        static int Fibbo(int n)
+        {
+            if (n == 0)
+            {
+                n = 0;
+                return n;
+            }
+            else if (n == 1)
+            {
+                n = 1;
+                return n;
+            }
+            return n = Fibbo(n - 1) + Fibbo(n - 2);
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Введите значение последовательности Фибонначи,чтобы получить число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Число из последовательности:{Fibbo(n)}");
+
         }
     }
 }
