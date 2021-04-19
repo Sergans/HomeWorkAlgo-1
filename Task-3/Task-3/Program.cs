45ee5d28c1a68208f6ceb7acc594e609b17ce2af
Сергей <gan7er@yandex.ru>
2021-04-19 14:54:40 +0300

    Тесты, конечный вариант

--- Task-3/Task-3/Program.cs
+++ Task-3/Task-3/Program.cs
@@ -4,6 +4,25 @@ namespace Task_3
 {
     class Program
     {
+       public class TestCase
+        {
+            public int nomberposition { get; set; }
+            public int sum { get; set; }
+        }
+        static void Test(TestCase testCase)
+        {
+            if (testCase.sum == Fibbo(testCase.nomberposition) && testCase.sum == FiboArray(testCase.nomberposition))
+            {
+                Console.WriteLine("VALID TEST");
+
+            }
+            else
+            {
+                Console.WriteLine("INVALID TEST");
+            }
+        }
+
+
         static int FiboArray(int n)
         {
             int f1 =0;
@@ -45,9 +64,15 @@ namespace Task_3
         }
         static void Main(string[] args)
         {
+            var testcase1 = new TestCase() { nomberposition = 1, sum = 1 };
+            var testcase2 = new TestCase() { nomberposition = 10, sum = 55 };
+            var testcase3 = new TestCase() { nomberposition = 22, sum = 17711 };
+            Test(testcase1);
+            Test(testcase2);
+            Test(testcase3);
             Console.WriteLine("Введите значение последовательности Фибонначи,чтобы получить число");
             int n = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine($"Число из последовательности:{Fibbo(n)}");
+            Console.WriteLine($"Число из последовательности:{Fibbo(n)}");
             Console.WriteLine($"Число из последовательности:{FiboArray(n)}");
         }
     }
